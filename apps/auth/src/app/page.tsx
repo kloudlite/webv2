2fedf974fb1090e1c7b4b178a139818a15cbd0b3
Karthik Th <karthik@kloudlite.io>
2025-04-03 09:52:58 +0530

    init

---
+++ apps/auth/src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Image from "next/image";
+import { Button } from '@kloudlite/design-system/atoms/button';
+
+
+export default function Home() {
+  return (
+    <div>
+      <Button content={"Hello"}/>
+    </div>
+  );
+}
